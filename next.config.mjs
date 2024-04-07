723c02cab62e259a7d9765d63017ece8ccb533ca
abhisek8094 <iasabhi41@gmail.com>
2024-04-07 09:05:17 +0530

    modify

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "upload.wikipedia.com",
+        hostname: "upload.wikipedia.org",
         pathname: "**",
       },
     ],
