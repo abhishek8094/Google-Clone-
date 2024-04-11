b6d5bf9601b3b2f414bddda78cfae0c72479860f
abhisek8094 <iasabhi41@gmail.com>
2024-04-11 06:57:23 +0530

    feat:CountryLoacation added

--- src/app/components/CountryLookup.js
+++ src/app/components/CountryLookup.js
@@ -6,15 +6,15 @@ export default function CountryLookup() {
   const [country, setCountry] = useState("India");
 
   const getCountry = async () => {
+    
     const response = await fetch(
       `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => data.country);
+      .then((data) => console.log(data));
     if (!response) return;
     setCountry(response);
   };
-  
 
   useEffect(() => {
     getCountry();
